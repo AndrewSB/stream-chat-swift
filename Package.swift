a7edf750544718ed938634f92cdd1680fd4547f4
Alexey Alter-Pesotskiy <a.alterpesotskiy@yahoo.com>
2022-10-05 10:04:50 +0100

    test

--- Package.swift
+++ Package.swift
@@ -60,7 +60,7 @@ let package = Package(
                 ],
                 path: "TestTools/StreamChatTestMockServer",
                 exclude: ["Info.plist"],
-                resources: [.copy("Fixtures/JSONs")]
+                resources: [.process("Fixtures")]
         ),
     ]
 )
