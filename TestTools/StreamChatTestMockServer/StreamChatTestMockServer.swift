b7cb94377f8b8346e11ab923c08406e083805b43
Alexey Alter-Pesotskiy <a.alterpesotskiy@yahoo.com>
2022-10-07 11:12:05 +0100

    test

--- TestTools/StreamChatTestMockServer/StreamChatTestMockServer.swift
+++ TestTools/StreamChatTestMockServer/StreamChatTestMockServer.swift
@@ -9,7 +9,7 @@ extension Bundle {
     private final class StreamChatTestMockServer {}
 
     static let bundleName = "StreamChat_StreamChatTestMockServer"
-    static let resourcesJSONs = "Fixtures/JSONs/"
+    static let resourcesJSONs = "TestTools/StreamChatTestMockServer/Fixtures/JSONs/"
 
     /// Returns the resource bundle associated with the current Swift module.
     static let testTools: Bundle = {
